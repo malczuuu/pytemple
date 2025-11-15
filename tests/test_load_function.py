ffafdaf9f0857af71b970e52a90e3fcfe384b5ba
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-11-15 12:30:02 +0100

    Add test for load from file function

---
+++ tests/test_load_function.py
@@ -0,0 +1,29 @@
+import os
+import re
+import tempfile
+
+import pytest
+
+import pytemple
+
+
+@pytest.fixture
+def temp_file():
+    tf = tempfile.NamedTemporaryFile(mode="w", delete=False)
+    tf.write("value=${random.int(10, 20)}")
+    tf.close()
+
+    yield tf.name
+
+    os.unlink(tf.name)
+
+
+@pytest.mark.repeat(10)
+def test_load_from_file_reads_and_processes(temp_file):
+    result = pytemple.load(temp_file)
+
+    m = re.search(r"value=(\d+)", result)
+    assert m is not None
+
+    val = int(m.group(1))
+    assert 10 <= val <= 20
